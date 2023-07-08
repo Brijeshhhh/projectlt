fec27ba40128c3381bd2d4e40859f63139ef5c86
AmalaJanalia <amalajanalia223@gmail.com>
2023-07-08 18:30:33 +0530

    js added

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+function clickEvent () {
+    
+    var text=document.getElementById("textField");
+        
+        text.style.display="block";
+        
+      
+
+} 
+function searchEvent () {
+    
+    var text=document.getElementById("textField1");
+        
+        text.style.display="block";
+        
+      
+        
+} 
