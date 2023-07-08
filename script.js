0a7ea56ab47904e2cf7dff2a63645e067ed481a5
AmalaJanalia <amalajanalia223@gmail.com>
2023-07-08 20:40:48 +0530

    modified:   index.html 	modified:   script.js

--- script.js
+++ script.js
@@ -1,41 +1,25 @@
+function clickEvent() {
+  var text = document.getElementById("textField");
 
-
-function clickEvent () {
-    
-    var text=document.getElementById("textField");
-        
-        text.style.display="block";
-        
-      
-
-} 
-function searchEvent () {
-    
-    var text=document.getElementById("textField1");
-        
-        text.style.display="block";
-        
-      
-        
-} 
-
-function validateForm () {
-    var nameip = document.forms[0]["name"];
-    var emailip = document.forms[0]["email"];
-    var messageip = document.forms[0]["message"];
-    if(nameip.value === ""){
-         alert("Name is required.");
-         return false;
-    }
-    if (emailip.value === "") {
-      alert("Email is required.");
-      return false;
-    }
-    if (messageip.value === "") {
-      alert("a message is required.");
-      return false;
-    }
-    return true;
-
+  text.style.display = "block";
 }
-    
+
+function validateForm() {
+  var nameip = document.forms[0]["name"];
+  var emailip = document.forms[0]["email"];
+  var messageip = document.forms[0]["message"];
+  if (nameip.value === "") {
+    alert("Name is required.");
+    return false;
+  }
+  if (emailip.value === "") {
+    alert("Email is required.");
+    return false;
+  }
+  if (messageip.value === "") {
+    alert("a message is required.");
+    return false;
+  } else {
+    alert("Thankyou for contacting us,we will reach at to you very soon");
+  }
+}
