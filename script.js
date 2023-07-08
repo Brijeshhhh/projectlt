0844f553ff6974eba8e44c242630504ba3c80de7
Brijeshhhh <94916359+Brijeshhhh@users.noreply.github.com>
2023-07-08 18:52:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,3 +18,24 @@ function searchEvent () {
       
         
 } 
+
+function validateForm () {
+    var nameip = document.forms[0]["name"];
+    var emailip = document.forms[0]["email"];
+    var messageip = document.forms[0]["message"];
+    if(nameip.value === ""){
+         alert("Name is required.");
+         return false;
+    }
+    if (emailip.value === "") {
+      alert("Email is required.");
+      return false;
+    }
+    if (messageip.value === "") {
+      alert("a message is required.");
+      return false;
+    }
+    return true;
+
+}
+    
